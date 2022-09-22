b08ce359e47c58f59e5c5ec60028d7bf6f27f02b
Im-Marlu <immarlux@gmail.com>
2022-09-22 19:19:50 +0200

    Uploading of project to GitHub

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,18 @@
+import "./card.styles.css";
+
+const Card = ({ monsterInformation }) => {
+  const { id, name, email } = monsterInformation;
+
+  return (
+    <div className="card-container">
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+export default Card;
